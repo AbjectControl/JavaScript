0bb64121150eef99d5d88eb103913d02e9728855
Abdul Moeez <warlordth8@gmail.com>
2024-12-15 09:00:53 -0800

    web dev

---
+++ 1st.js
@@ -0,0 +1,25 @@
+console.log("HAHAHA");
+let name="Abdul Moeez";
+console.log(name);
+
+
+console.log(typeof name);
+const student = {
+    name : "Abdul Moeez",
+    age : 18,
+};
+//let , var , const
+
+console.log(student);
+student.age=21; 
+console.log(student.age);//onsole.log(student["age"]);
+
+
+
+
+
+
+
+
+
+
---
+++ 2nd.js
@@ -0,0 +1,15 @@
+//Arthimetic operators
+let a =5;
+let b =2;
+console.log("a =",a,"b =",b);
+console.log("a/b",a/b);
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a%b",a%b);
+console.log("a*b",a*b);
+console.log("a**b",a**b);//this is a ki power b
+
+//Unary Operators
+console.log(a++);
+console.log(b++);
+console.log("a =",a,"b =",b);
